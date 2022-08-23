892ef8e0e5aafa1b9145e7ff3ed129de28b3a095
Chandu Sri Vasavi <94160626+SriVasavi-7@users.noreply.github.com>
2022-08-23 18:33:51 +0530

    Update Code.py

--- TASK 1- Face Detection/Code.py
+++ TASK 1- Face Detection/Code.py
@@ -1,8 +1,6 @@
 import pathlib
 import cv2
-
-cascade_path=pathlib.Path(cv2._file_).parent.absolute() / "data/haarcascade_frontalface_default.xml"
-#print(cascade_path)
+cascade_path=pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 clf=cv2.CascadeClassifier(str(cascade_path))
 camera=cv2.VideoCapture(0)
 while True:
